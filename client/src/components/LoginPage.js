2de9d6e90f857efbb95c8bc027621870498ad363
Calypso Leonard <calypsorleonard@gmail.com>
2022-05-09 15:52:39 -0400

    sign up form wireframe

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -11,7 +11,7 @@ function LoginPage({goodLogin}) {
 
     function handleSubmit(e) {
         e.preventDefault()
-        fetch("http://localhost:3000/login", {
+        fetch("http://localhost:4000/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -1,8 +1,40 @@
 function SignUpForm() {
     
+    function handleSubmit(e){
+        e.preventDefault()
+    }
     return (
-        <div>
-            <h3>sign up here!</h3>
+        <div className="signup_form">
+            <h3>Welcome!</h3>
+            <p>Fill out your details below to sign up:</p>
+            <form onSubmit={handleSubmit}>
+                <label>username</label>
+                <input
+                    type="text"
+                    name="username"
+                />
+                <br/>
+                <label>email address</label>
+                <input
+                    type="text"
+                    name="email"
+                />
+                <br/>
+                <label>password</label>
+                <input
+                    type="password"
+                    name="password"
+                />
+                <br/>
+                <label>confirm password</label>
+                <input
+                    type="password"
+                    name="password_confirm"
+                />
+                <br/>
+
+                <button type="submit">Sign up!</button>
+            </form>
         </div>
     )
 }
